1cdd65876c922c5d3e38f6702ce373e6c0f6ecb8
AlanDecode <zytbuaa1415@gmail.com>
2019-02-15 00:06:31 +0800

    🚀 perf: 优化提示语

--- assets/VOID.js
+++ assets/VOID.js
@@ -47,36 +47,31 @@ var VOID = {
                 text = '嗨！来自 ' + referrer.hostname + ' 的朋友！';
                 var domain = referrer.hostname.split('.')[1];
                 if (domain == 'baidu') {
-                    text = '嗨！ 来自 百度搜索 的朋友！欢迎访问 ' + document.title.split(' - ')[0];
+                    text = '嗨！ 来自 百度搜索 的朋友！';
                 } else if (domain == 'so') {
-                    text = '嗨！ 来自 360搜索 的朋友！欢迎访问 ' + document.title.split(' - ')[0];
+                    text = '嗨！ 来自 360搜索 的朋友！';
                 } else if (domain == 'google') {
-                    text = '嗨！ 来自 谷歌搜索 的朋友！欢迎访问 ' + document.title.split(' - ')[0];
+                    text = '嗨！ 来自 谷歌搜索 的朋友！';
                 }
-            } else {
-                if (window.location.href == VOIDConfig.home) {
-                    var now = (new Date()).getHours();
-                    if (now > 23 || now <= 5) {
-                        text = '你是夜猫子呀？这么晚还不睡觉，明天起的来嘛？';
-                    } else if (now > 5 && now <= 7) {
-                        text = '早上好！一日之计在于晨，美好的一天就要开始了！';
-                    } else if (now > 7 && now <= 11) {
-                        text = '上午好！工作顺利嘛，不要久坐，多起来走动走动哦！';
-                    } else if (now > 11 && now <= 14) {
-                        text = '中午了，工作了一个上午，现在是午餐时间！';
-                    } else if (now > 14 && now <= 17) {
-                        text = '午后很容易犯困呢，今天的运动目标完成了吗？';
-                    } else if (now > 17 && now <= 19) {
-                        text = '傍晚了！窗外夕阳的景色很美丽呢，最美不过夕阳红~';
-                    } else if (now > 19 && now <= 21) {
-                        text = '晚上好，今天过得怎么样？';
-                    } else if (now > 21 && now <= 23) {
-                        text = '已经这么晚了呀，早点休息吧，晚安~';
-                    } 
-                } else {
-                    text = '欢迎阅读 ' + document.title.split(' - ')[0];
-                }
-            }
+            } 
+            var now = (new Date()).getHours();
+            if (now > 23 || now <= 5) {
+                text = text + '你是夜猫子呀？这么晚还不睡觉，明天起的来嘛？';
+            } else if (now > 5 && now <= 7) {
+                text = text + '早上好！一日之计在于晨，美好的一天就要开始了！';
+            } else if (now > 7 && now <= 11) {
+                text = text + '上午好！工作顺利嘛，不要久坐，多起来走动走动哦！';
+            } else if (now > 11 && now <= 14) {
+                text = text + '中午了，工作了一个上午，现在是午餐时间！';
+            } else if (now > 14 && now <= 17) {
+                text = text + '午后很容易犯困呢，今天的运动目标完成了吗？';
+            } else if (now > 17 && now <= 19) {
+                text = text + '傍晚了！窗外夕阳的景色很美丽呢，最美不过夕阳红~';
+            } else if (now > 19 && now <= 21) {
+                text = text + '晚上好，今天过得怎么样？';
+            } else if (now > 21 && now <= 23) {
+                text = text + '已经这么晚了呀，早点休息吧，晚安~';
+            } 
             setTimeout(function() {
                 alert(text);
             }, 200);
@@ -214,7 +209,9 @@ var VOID = {
         VOID.reload();
         VOID.handleLike();
         AjaxComment.init();
-        alert('欢迎阅读 ' + document.title.split(' - ')[0]);
+        if(VOIDConfig.welcomeWord){
+            alert('欢迎访问 ' + document.title);
+        }
     },
 
     // 重载与事件绑定
