e172c2b5721b378c098fe225bc2b3525cbb2d10b
AlanDecode <zytbuaa1415@gmail.com>
2019-08-21 10:24:54 +0800

    feat: apple web app

--- includes/head.php
+++ includes/head.php
@@ -25,6 +25,8 @@ if (isset($_POST['void_action'])) {
     <meta name="renderer" content="webkit">
     <meta name="HandheldFriendly" content="true">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <?php 
     $banner = '';
     $description = '';
