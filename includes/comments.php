bb114e5450b6e3a71490e1e061aff2e298ad218a
AlanDecode <zytbuaa1415@gmail.com>
2019-09-08 16:01:37 +0800

    perf: 调整评论区提示与 notice 样式

--- includes/comments.php
+++ includes/comments.php
@@ -33,16 +33,29 @@ $setting = $GLOBALS['VOIDSetting'];
             <?php endif; ?>
             <form method="post" action="<?php $this->commentUrl() ?>" id="comment-form">
                 <?php if($this->user->hasLogin()): ?>
-                <p id="logged-in" data-name="<?php $this->user->screenName(); ?>" data-url="<?php $this->user->url(); ?>" data-email="<?php $this->user->mail(); ?>" ><?php _e('登录身份: '); ?><a href="<?php $this->options->profileUrl(); ?>"><?php $this->user->screenName(); ?></a>. <a no-pjax href="<?php $this->options->logoutUrl(); ?>" title="Logout"><?php _e('退出'); ?> &raquo;</a></p>
+                <p id="logged-in" 
+                    data-name="<?php $this->user->screenName(); ?>" 
+                    data-url="<?php $this->user->url(); ?>" 
+                    data-email="<?php $this->user->mail(); ?>" ><?php _e('登录身份: '); ?>
+                    <a href="<?php $this->options->profileUrl(); ?>"><?php $this->user->screenName(); ?></a>
+                    . <a no-pjax href="<?php $this->options->logoutUrl(); ?>" title="Logout"><?php _e('退出'); ?> &raquo;</a>
+                </p>
                 <?php else: ?>
                     <div class="comment-info-input">
                     <input aria-label="称呼(必填)" type="text" name="author" id="author" placeholder="称呼(必填)" value="<?php $this->remember('author'); ?>" />
-                    <input aria-label="电子邮件<?php echo Helper::options()->commentsRequireMail? '(必填，将保密)' : '(选填)' ?>" type="email" name="mail" id="mail" placeholder="电子邮件<?php echo Helper::options()->commentsRequireMail? '(必填，将保密)' : '(选填)' ?>" value="<?php $this->remember('mail'); ?>" />
-                    <input aria-label="网站(选填)" type="url" name="url" id="url" placeholder="网站<?php echo Helper::options()->commentsRequireURL? '(必填)' : '(选填)' ?>"  value="<?php $this->remember('url'); ?>" />
+                    <input aria-label="电子邮件<?php echo Helper::options()->commentsRequireMail? '(必填，将保密)' : '(选填)' ?>" 
+                        type="email" name="mail" id="mail" 
+                        placeholder="电子邮件<?php echo Helper::options()->commentsRequireMail? '(必填，将保密)' : '(选填)' ?>" 
+                        value="<?php $this->remember('mail'); ?>" />
+                    <input aria-label="网站(选填)" type="url" name="url" id="url" 
+                        placeholder="网站<?php echo Helper::options()->commentsRequireURL? '(必填)' : '(选填)' ?>"  
+                        value="<?php $this->remember('url'); ?>" />
                     </div>
                 <?php endif; ?>
                 <p style="margin-top:0">
-                    <textarea aria-label="评论输入框" class="input-area" rows="5" name="text" id="textarea" placeholder="在这里输入你的评论..." style="resize:none;"><?php $this->remember('text'); ?></textarea>
+                    <textarea aria-label="评论输入框" class="input-area" rows="5" name="text" id="textarea" 
+                        placeholder="在这里输入你的评论..." 
+                        style="resize:none;"><?php $this->remember('text'); ?></textarea>
                 </p>
                 <p class="comment-buttons">
                     <span class="OwO" aria-label="表情按钮" role="button"></span>
