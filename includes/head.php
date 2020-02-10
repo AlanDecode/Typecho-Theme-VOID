84ebab48901dea8fe42143a663f0d7f18e6ce655
AlanDecode <AlanDecode@users.noreply.github.com>
2020-02-10 04:19:45 +0000

    Automated deployment: Mon Feb 10 04:19:45 UTC 2020 c29111e5c17f76488250d1cd2c03b16206a2862a

--- includes/head.php
+++ includes/head.php
@@ -56,7 +56,7 @@ if (isset($_POST['void_action'])) {
 
     <!--CSS-->
     <link rel="stylesheet" href="<?php Utils::indexTheme('/assets/bundle-1e9bf597b1.css');?>">
-    <link rel="stylesheet" href="<?php Utils::indexTheme('/assets/VOID-5332e4959d.css');?>">
+    <link rel="stylesheet" href="<?php Utils::indexTheme('/assets/VOID-11861506b2.css');?>">
 
     <!--JS-->
     <script src="<?php Utils::indexTheme('/assets/bundle-header-9977c8fbf5.js'); ?>"></script>
