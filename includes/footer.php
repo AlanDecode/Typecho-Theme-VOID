3e9ab61a9dd03d4e05f47ba7af4a75fdb8898960
AlanDecode <AlanDecode@users.noreply.github.com>
2020-02-13 07:28:06 +0000

    Automated deployment: Thu Feb 13 07:28:06 UTC 2020 89cdb097adc2f32e9e9ff5c87cc694659a28f57c

--- includes/footer.php
+++ includes/footer.php
@@ -134,7 +134,7 @@ $setting = $GLOBALS['VOIDSetting'];
         <?php if($setting['enableMath']): ?>
         <script src='<?php Utils::indexTheme('/assets/libs/mathjax/2.7.4/MathJax.js'); ?>'></script>
         <?php endif; ?>
-        <script src="<?php Utils::indexTheme('/assets/VOID-c0249d6333.js'); ?>"></script>
+        <script src="<?php Utils::indexTheme('/assets/VOID-2c818e2660.js'); ?>"></script>
         <script>
         if($(".OwO").length > 0){
             new OwO({
--- includes/head.php
+++ includes/head.php
@@ -56,7 +56,7 @@ if (isset($_POST['void_action'])) {
 
     <!--CSS-->
     <link rel="stylesheet" href="<?php Utils::indexTheme('/assets/bundle-1e9bf597b1.css');?>">
-    <link rel="stylesheet" href="<?php Utils::indexTheme('/assets/VOID-0c58488ec9.css');?>">
+    <link rel="stylesheet" href="<?php Utils::indexTheme('/assets/VOID-8792d9ed69.css');?>">
 
     <!--JS-->
     <script src="<?php Utils::indexTheme('/assets/bundle-header-9977c8fbf5.js'); ?>"></script>
